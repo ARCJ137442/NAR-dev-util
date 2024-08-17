14b9beb2b16857955dfbe667811a74d8df97ab7b
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2024-08-17 23:11:27 +0800

    refactor: :recycle: ♻️使用「图式trait+NewLine图式」统一「带所有权拆分迭代器」；🈲因「难以调和的不一致」禁止空字符串；✅增添单元测试

--- src/str_processing/split_owned.rs
+++ src/str_processing/split_owned.rs
@@ -9,15 +9,30 @@
 ///   * ⚠️对于`&[char]`无法确定「选中的是哪个[`char`]」因此导致「无法确认选中的图式长度」
 /// * ✨后续可扩展，或直接基于稳定后的[`Pattern`](crate::str::Pattern)特征加入
 pub trait PatternWithLen {
+    /// 获取第一个匹配字符的索引位置和长度
     fn find_with_len(&self, haystack: &str) -> Option<(usize, usize)>;
+
+    /// 是否忽略最后一个空子串
+    /// * 🎯同时适配「拆分行」与「拆分普通图式」
+    ///   * 📄「拆分行」在`"abc\n"`仅拆分出`["abc"]`而不会拆出`""`
+    /// * 📜默认为否：禁用此规则
+    const IGNORE_FINAL_EMPTY: bool = false;
 }
 
 /// 统一对闭包实现
 /// * 📌【2024-08-17 21:57:27】此处基于可copy的[`Fn`]
 impl<F: Fn(char) -> bool> PatternWithLen for F {
     fn find_with_len(&self, haystack: &str) -> Option<(usize, usize)> {
-        haystack.find(self).map(|i| (i, haystack[i..=i].len()))
+        match haystack.find(self) {
+            // * 🚩长度：获取字符串在索引i之后第一个字符的长度
+            Some(i) => Some((i, next_char_len(haystack, i)?)),
+            None => None,
+        }
     }
+}
+
+fn next_char_len(haystack: &str, i: usize) -> Option<usize> {
+    Some(haystack[i..].chars().next()?.len_utf8())
 }
 
 impl PatternWithLen for char {
@@ -28,7 +43,39 @@ impl PatternWithLen for char {
 
 impl PatternWithLen for &str {
     fn find_with_len(&self, haystack: &str) -> Option<(usize, usize)> {
+        // ! ❌【2024-08-17 22:57:19】禁用空字串的使用
+        // * 🔗参考：<https://github.com/rust-lang/rust/issues/33882>
+        assert!(!self.is_empty(), "Empty pattern is not allowed. Discussions see <https://github.com/rust-lang/rust/issues/33882>");
         haystack.find(self).map(|i| (i, self.len()))
+    }
+}
+
+/// 用于作为「换行」的搜索图式
+#[derive(Debug, Clone, Copy)]
+pub struct NewLine;
+
+impl PatternWithLen for NewLine {
+    const IGNORE_FINAL_EMPTY: bool = true;
+    /// 参照[`core::str::Lines`]内部的`LinesMap`（私有）制作
+    /// * 📌返回`(起始索引, 子串长度)`
+    /// * 🚩先拿到换行`\n`，然后试着到回头拿回车`\r`
+    fn find_with_len(&self, haystack: &str) -> Option<(usize, usize)> {
+        const LEN_CR: usize = "\r".len();
+        const LEN_LF: usize = "\n".len();
+        const LEN_CRLF: usize = "\r\n".len();
+        // 先拿到换行符索引
+        let lf_index = haystack.find('\n')?;
+        if lf_index >= LEN_CR {
+            // 若有可能，尝试拿回车符
+            let cr_index = lf_index - LEN_CR;
+            // ⚠️此处单凭相减得到的索引，可能不是合法UTF-8位置
+            if haystack.is_char_boundary(cr_index) && haystack[cr_index..lf_index] == *"\r" {
+                // 换行回车
+                return Some((cr_index, LEN_CRLF));
+            }
+        }
+        // 不然只有换行
+        Some((lf_index, LEN_LF))
     }
 }
 
@@ -50,6 +97,15 @@ impl<Pattern: PatternWithLen> Iterator for IterSplitCharOwned<Pattern> {
         let residual = self.residual.as_mut()?;
         // 寻找下一个换行符
         let mut new_residual = match self.pattern.find_with_len(residual) {
+            // 空字串情况⇒单独处理
+            // * 🚩降级为「遍历所有字符」
+            // ! ❌仍然无法与`str::split`匹配
+            //   * 🚩【2024-08-17 22:54:59】目前选择「注释掉逻辑&panic」禁止此情形
+            //   * 🔗参考：<https://github.com/rust-lang/rust/issues/33882>
+            // Some((index_begin_of_delim, 0)) => {
+            //     let index_next_char = next_char_len(residual, index_begin_of_delim)?;
+            //     residual.split_off(index_next_char)
+            // }
             Some((index_begin_of_delim, len_delim)) => {
                 let index_end_of_delim = index_begin_of_delim + len_delim;
                 let new_residual = residual.split_off(index_end_of_delim);
@@ -61,76 +117,8 @@ impl<Pattern: PatternWithLen> Iterator for IterSplitCharOwned<Pattern> {
         };
         // 将剩余的字符串移动到 residual 中
         std::mem::swap(residual, &mut new_residual);
-        // 获取并返回被截去的字符串
-        let splitted_out = new_residual;
-        Some(splitted_out)
-    }
-}
-
-/// 用于「根据指定字符拆分字符串」的迭代器
-/// * 🔗参考：<https://www.reddit.com/r/rust/comments/qxcp1w/why_cant_you_split_a_string>
-#[derive(Debug, Clone)]
-#[must_use = "iterators are lazy and do nothing unless consumed"]
-pub struct LinesOwned {
-    /// 剩余的字符串
-    residual: Option<String>,
-}
-
-impl LinesOwned {
-    /// 参照[`core::str::Lines`]内部的`LinesMap`（私有）制作
-    /// * 📌返回`(起始索引, 子串长度)`
-    /// * 🚩先拿到换行`\n`，然后试着到回头拿回车`\r`
-    fn find_delim(s: &str) -> Option<(usize, usize)> {
-        const LEN_CR: usize = "\r".len();
-        const LEN_LF: usize = "\n".len();
-        const LEN_CRLF: usize = "\r\n".len();
-        // 先拿到换行符索引
-        let lf_index = s.find('\n')?;
-        if lf_index >= LEN_CR {
-            // 若有可能，尝试拿回车符
-            let cr_index = lf_index - LEN_CR;
-            // ⚠️此处单凭相减得到的索引，可能不是合法UTF-8位置
-            if s.is_char_boundary(cr_index) && s[cr_index..lf_index] == *"\r" {
-                // 换行回车
-                return Some((cr_index, LEN_CRLF));
-            }
-        }
-        // 不然只有换行
-        Some((lf_index, LEN_LF))
-    }
-}
-
-impl Iterator for LinesOwned {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let residual = self.residual.as_mut()?;
-        // 寻找下一个换行符
-        let mut new_residual = match Self::find_delim(residual) {
-            Some((index_begin_of_delim, len_delim)) => {
-                let index_end_of_delim = index_begin_of_delim + len_delim;
-                debug_assert!(
-                    residual.is_char_boundary(index_end_of_delim),
-                    "不会发生：find_delim在{residual:?}中找到的索引{index_begin_of_delim}应该在合法UTF-8位置"
-                );
-                let new_residual = residual.split_off(index_end_of_delim);
-                residual.truncate(index_begin_of_delim);
-                new_residual
-            }
-            // 没分隔符了⇒返回自身所持有的字符串
-            // * ✅应对"abc\n"的情况也不会「当作两行」：
-            //   * 当「拆分出"abc"」之后，`residual`就会被置空
-            None => {
-                return self.residual.take();
-                // return match self.residual.take() {
-                //     Some(x) if x.is_empty() => None,
-                //     x => x,
-                // };
-            }
-        };
-        // 将剩余的字符串移动到 residual 中
-        std::mem::swap(residual, &mut new_residual);
-        if residual.is_empty() {
+        if Pattern::IGNORE_FINAL_EMPTY && residual.is_empty() {
+            // 剩余的字符串为空，则直接返回
             self.residual = None;
         }
         // 获取并返回被截去的字符串
@@ -146,25 +134,52 @@ impl Iterator for LinesOwned {
 pub trait SplitOwned: Sized {
     /// 以某个固定的字符分隔字符串
     /// * 🎯[`str::split`]的带所有权版本（不完整）
+    ///
+    /// # Panics
+    ///
+    /// ❌【2024-08-17 22:59:51】目前**禁止输入空字符串** `""`：效果与对应[`str::split`]不一致，且使用场合少
+    /// * ⚠️Empty `&str` as pattern is forbidden. Otherwise, the program will panic.
+    /// * 🚩建议：在传入该方法前预先判空
+    /// * 🚧后续若有使用需求，才考虑加入
+    /// * 📌主要堵点：[`str::split`]不一致的「前后空白子串」
+    ///   * 🔗参考：<https://github.com/rust-lang/rust/issues/33882>
+    ///
+    /// ## Example
+    ///
+    /// cloned = `["", "中", "文", "1", "2", "3", "🤣", "👉", "⇑", "🤡", "↑", "\n", "E", "n", "g", "l", "i", "s", "h", "😆", "\n", "あ", "💭", "t", "h", "i", "s", "\n", "Y", "o", "u", "!", "\r", "\n", "\t", " ", "\u{12}", "1", "\n", ""]`
+    /// !=
+    /// owned = `["中", "文", "1", "2", "3", "🤣", "👉", "⇑", "🤡", "↑", "\n", "E", "n", "g", "l", "i", "s", "h", "😆", "\n", "あ", "💭", "t", "h", "i", "s", "\n", "Y", "o", "u", "!", "\r", "\n", "\t", " ", "\u{12}", "1", "\n"]`
     fn split_owned<Pattern: PatternWithLen>(self, pat: Pattern) -> impl Iterator<Item = String>;
 
     /// 带所有权地拆分字符串的行
     /// * 🎯无空间开销地拆分字符串
     ///   * 📄场景：一个数十Kb级大小的JSON文本要拆成两行，需要尽可能避免内容复制
     /// * ⚡可避免拷贝字符串
-    fn lines_owned(self) -> impl Iterator<Item = String>;
+    fn lines_owned(self) -> impl Iterator<Item = String> {
+        self.split_owned(NewLine)
+    }
 
     /// 带所有权地拆分字符串一次
     /// * 🎯无空间开销拆分字符串为两半
     /// * 🚩默认拆分从左往右（索引从小到大）第一个图式
     ///   * 📌若未找到图式，则返还自身
     /// * ⚡可避免拷贝字符串
+    ///
+    /// # Panics
+    ///
+    /// ❌【2024-08-17 22:59:51】目前禁止输入**空字符串**，因效果与对应[`str::split`]不一致
+    /// * ⚠️Empty `&str` as pattern is forbidden. Otherwise, the program will panic.
+    /// * 🚧后续若有使用需求，才考虑加入
+    /// * 📌主要堵点：[`str::split`]不一致的「前后空白子串」
+    ///   * 🔗参考：<https://github.com/rust-lang/rust/issues/33882>
     fn split_owned_once<Pattern: PatternWithLen>(self, pat: Pattern) -> Result<(Self, Self), Self>;
 
     /// 带所有权地按行拆分字符串一次
     /// * 🎯无空间开销拆分字符串为两行
     /// * 📄参考[`SplitOwned::split_char_once_owned`]
-    fn split_ln_owned_once(self) -> Result<(Self, Self), Self>;
+    fn split_line_owned_once(self) -> Result<(Self, Self), Self> {
+        self.split_owned_once(NewLine)
+    }
 }
 
 impl SplitOwned for String {
@@ -200,31 +215,6 @@ impl SplitOwned for String {
             None => Err(self),
         }
     }
-
-    fn lines_owned(self) -> impl Iterator<Item = String> {
-        LinesOwned {
-            residual: Some(self),
-        }
-    }
-
-    fn split_ln_owned_once(mut self) -> Result<(Self, Self), Self> {
-        match LinesOwned::find_delim(&self) {
-            Some((index_begin_of_delim, len_delim)) => {
-                let index_end_of_delim = index_begin_of_delim + len_delim;
-                debug_assert!(
-                    self.is_char_boundary(index_end_of_delim),
-                    "不会发生：find_delim在{self:?}中找到的索引{index_begin_of_delim}应该在合法UTF-8位置"
-                );
-                // 拆分出剩余字符串
-                let right = self.split_off(index_begin_of_delim + len_delim);
-                // 抛掉分隔符
-                self.truncate(index_begin_of_delim);
-                Ok((self, right))
-            }
-            // 没分隔符了⇒返回自身所持有的字符串
-            None => Err(self),
-        }
-    }
 }
 
 #[cfg(test)]
@@ -233,7 +223,7 @@ mod tests {
     use crate::{f_tensor, macro_once};
 
     #[test]
-    fn split_char_owned() {
+    fn split_owned_char() {
         fn test(c: char, s: impl ToString) {
             let s = s.to_string();
             // 一次拆分
@@ -266,6 +256,83 @@ mod tests {
     }
 
     #[test]
+    fn split_owned_ref_str() {
+        fn test(c: &str, s: impl ToString) {
+            let s = s.to_string();
+            // 一次拆分
+            let cloned_split = s
+                .split_once(c)
+                .map(|(a, b)| (a.to_owned(), b.to_owned()))
+                .ok_or_else(|| s.to_owned());
+            let owned_split = s.clone().split_owned_once(c);
+            assert_eq!(
+                cloned_split, owned_split,
+                "两种方式拆分不等：\ncloned = {cloned_split:?}\n!=\nowned = {owned_split:?}\nc = {c:?}"
+            );
+            // 多次拆分
+            let cloned_split = s.split(c).map(ToString::to_string).collect::<Vec<_>>();
+            let owned_split = s.clone().split_owned(c).take(0xff).collect::<Vec<_>>();
+            assert_eq!(
+                cloned_split, owned_split,
+                "两种方式拆分不等：\ncloned = {cloned_split:?}\n!=\nowned = {owned_split:?}\nc = {c:?}"
+            );
+        }
+        f_tensor! {
+            test;
+            "\r" "\n" "\r\n" "\t" /* "" */ "🤣" "n";
+            "中文123🤣👉⇑🤡↑\nEnglish😆\nあ💭this\nYou!\r\n\t \x121\n"
+            "r \r n \n rn \r\n换行最后有内容"
+            "换行最后无内容\r"
+            "换行最后无内容\n"
+            "换行最后无内容\r\n"
+        };
+    }
+
+    /// 禁止对空字符串展开迭代
+    #[test]
+    #[should_panic]
+    fn empty_str_pattern_is_forbidden() {
+        for _ in "abc".to_string().split_owned("") {}
+    }
+
+    #[test]
+    fn split_owned_fn() {
+        fn test(pat: impl Fn(char) -> bool, s: impl ToString) {
+            let s = s.to_string();
+            // 一次拆分
+            let cloned_split = s
+                .split_once(&pat)
+                .map(|(a, b)| (a.to_owned(), b.to_owned()))
+                .ok_or_else(|| s.to_owned());
+            let owned_split = s.clone().split_owned_once(&pat);
+            assert_eq!(
+                cloned_split, owned_split,
+                "两种方式拆分不等：\ncloned = {cloned_split:?}\n!=\nowned = {owned_split:?}"
+            );
+            // 多次拆分
+            let cloned_split = s.split(&pat).map(ToString::to_string).collect::<Vec<_>>();
+            let owned_split = s.clone().split_owned(&pat).take(0xff).collect::<Vec<_>>();
+            assert_eq!(
+                cloned_split, owned_split,
+                "两种方式拆分不等：\ncloned = {cloned_split:?}\n!=\nowned = {owned_split:?}"
+            );
+        }
+        f_tensor! {
+            test;
+            char::is_whitespace
+            char::is_alphabetic
+            char::is_alphanumeric,
+            { |c:char| c.is_ascii() }
+            ;
+            "中文123🤣👉⇑🤡↑\nEnglish😆\nあ💭this\nYou!\r\n\t \x121\n"
+            "r \r n \n rn \r\n换行最后有内容"
+            "换行最后无内容\r"
+            "换行最后无内容\n"
+            "换行最后无内容\r\n"
+        };
+    }
+
+    #[test]
     fn lines_owned() {
         fn test(s: impl ToString) {
             let s = s.to_string();
@@ -282,7 +349,7 @@ mod tests {
                 };
                 Ok((s[..left_i].to_owned(), s[i_lf + 1..].to_owned()))
             };
-            let owned_split = s.clone().split_ln_owned_once();
+            let owned_split = s.clone().split_line_owned_once();
             assert_eq!(
                 cloned_split, owned_split,
                 "两种方式拆分不等：\ns = {s:?}\ncloned = {cloned_split:?}\n!=\nowned = {owned_split:?}"
